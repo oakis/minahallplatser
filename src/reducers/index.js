e2c755b9819d7391003d240fdf66daa798d61258
oakis <oakismen@gmail.com>
2017-08-09 20:47:09 +0200

    clean reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,12 @@
 import { combineReducers } from 'redux';
-import { Reducer } from 'react-native-router-flux';
 import AuthReducer from './AuthReducer';
 import FavReducer from './FavReducer';
 import searchReducer from './searchReducer';
 import DepartureReducer from './DepartureReducer';
 
-export const RouteReducer = params => {
-    const defaultReducer = Reducer(params);
-    return (state, action) => {
-        console.log('ACTION:', action);
-        return defaultReducer(state, action);
-    };
-};
-
 export default combineReducers({
 	auth: AuthReducer,
 	fav: FavReducer,
 	search: searchReducer,
-	departures: DepartureReducer,
-    routes: RouteReducer
+	departures: DepartureReducer
 });
