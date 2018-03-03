0040dfdfff680e7031a224d3145996657d13240f
Anders Ekman <oakismen@gmail.com>
2018-03-03 22:30:40 +0100

    start tracking to fabric

--- src/components/helpers/mixpanel.js
+++ src/components/helpers/mixpanel.js
@@ -1,4 +1,5 @@
 import Mixpanel from 'react-native-mixpanel';
+import { Answers } from 'react-native-fabric';
 import { mixpanelKey } from '../../Variables';
 
 Mixpanel.sharedInstanceWithToken(mixpanelKey);
@@ -7,8 +8,10 @@ export const track = (title, props) => {
     if (props) {
         window.log(`Mixpanel track with props: ${title}`, props);
         Mixpanel.trackWithProperties(title, props);
+        Answers.logCustom(title, props);
     } else {
         window.log(`Mixpanel track: ${title}`);
         Mixpanel.track(title);
+        Answers.logCustom(title);
     }
 };
