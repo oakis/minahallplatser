e2bece9967dcb88f047fa65a8f925a8322700e69
Anders Ekman <oakismen@gmail.com>
2019-08-19 15:31:26 +0200

    connect redux dev tools

--- src/setupStore.js
+++ src/setupStore.js
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import ReduxThunk from 'redux-thunk';
 import rootReducer from './reducers';
@@ -14,11 +14,15 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default () => {
     const store = createStore(
         persistedReducer,
         {},
-        applyMiddleware(...middleware)
+        composeEnhancers(
+            applyMiddleware(...middleware),
+        ),
     );
     const persistor = persistStore(store);
     return { store, persistor };
