b452e0a3292493172401a135c4e0d1fd1f83bc87
Anders Ekman <oakismen@gmail.com>
2021-01-30 20:12:48 +0100

    use hooks for App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import React, { Component } from 'react';
-import { StatusBar } from 'react-native';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import Router from './Router';
-import SplashScreen from './components/SplashScreen';
-import factory from './setupStore';
-import { track } from './components/helpers';
-import { colors } from './components/style';
+import React, { useEffect } from "react";
+import { StatusBar } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import Router from "./Router";
+import SplashScreen from "./components/SplashScreen";
+import factory from "./setupStore";
+import { track } from "./components/helpers";
+import { colors } from "./components/style";
 
 export const { store, persistor } = factory();
 
-class App extends Component {
+const App = () => {
+  useEffect(() => {
+    track("App Start");
+  });
 
-  componentDidMount() {
-    track('App Start');
-  }
-
-  render() {
-    return (
-      <Provider store={store}>
-        <PersistGate loading={<SplashScreen />} persistor={persistor}>
-          <StatusBar
-            backgroundColor={colors.primary}
-            barStyle="light-content"
-          />
-          <Router />
-        </PersistGate>
-      </Provider>
-    );
-  }
-
-}
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<SplashScreen />} persistor={persistor}>
+        <StatusBar backgroundColor={colors.primary} barStyle="light-content" />
+        <Router />
+      </PersistGate>
+    </Provider>
+  );
+};
 
 export default App;
