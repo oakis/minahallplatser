ccb90b82226e9a9e84f3ed4a32a5b6af96bac5f1
Anders Ekman <oakismen@gmail.com>
2022-05-18 10:45:13 +0200

    type logging

--- src/setupLogging.ts
+++ src/setupLogging.ts
@@ -1,22 +1,32 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import {LogBox} from 'react-native';
 
-type StartTimesType = Record<string, unknown>;
+interface CustomWindow extends Window {
+  timeStart: (value: string) => void;
+  timeEnd: (value: string) => void;
+  log: (...messages: string[]) => void;
+}
+declare let window: CustomWindow;
 
-const startTimes: StartTimesType = {};
+interface CustomGlobal extends Global {
+  performance: Record<string, () => number>;
+}
+declare let global: CustomGlobal;
+
+const startTimes: Record<string, number> = {};
 
 if (__DEV__) {
   window.log = console.log.bind(console);
   window.timeStart = (label: string): void => {
     startTimes[label] = global.performance.now();
   };
-  window.timeEnd = label => {
+  window.timeEnd = (label: string): void => {
     const endTime = global.performance.now();
     if (startTimes[label]) {
       const delta = endTime - startTimes[label];
       window.log(`${label}: ${delta.toFixed(3)}ms`);
       delete startTimes[label];
-      return delta;
+      return;
     }
     console.warn(`Warning: No such label '${label}' for window.timeEnd()`);
   };
